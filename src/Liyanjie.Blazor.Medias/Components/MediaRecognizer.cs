20f46a99c0d27d829d8b3c1a3a7ea802be1033f6
李發財 <liyanjie8712@outlook.com>
2021-11-21 16:33:07 +0800

    +

---
+++ src/Liyanjie.Blazor.Medias/Components/MediaRecognizer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Liyanjie.Blazor.Medias.Components;
+
+public class MediaRecognizer : ComponentBase
+{
+    [Inject] JsInterop? JsInterop { get; set; }
+    [Parameter] public string? MediaId { get; set; }
+
+    public ValueTask<MediaObject>? Get() => JsInterop?.Get(MediaId);
+    public void Play()
+    {
+        _ = (JsInterop?.Play(MediaId));
+    }
+
+    public void Pause()
+    {
+        _ = (JsInterop?.Pause(MediaId));
+    }
+
+    public void SetMuted(bool muted)
+    {
+        _ = (JsInterop?.SetMuted(MediaId, muted));
+    }
+
+    public void SetLoop(bool loop)
+    {
+        _ = (JsInterop?.SetLoop(MediaId, loop));
+    }
+
+    public void SetCurrentTime(int currentTime)
+    {
+        _ = (JsInterop?.SetCurrentTime(MediaId, currentTime));
+    }
+
+    public void SetVolume(double volume)
+    {
+        _ = (JsInterop?.SetVolume(MediaId, volume));
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        _ = (JsInterop?.SetFullscreen(MediaId, fullscreen));
+    }
+}
+
