a7d396a9ae27aade7586e97877d0c80369674002
李發財 <liyanjie8712@outlook.com>
2021-12-19 02:21:43 +0800

    +

--- src/Liyanjie.Blazor.Medias/wwwroot/js.js
+++ src/Liyanjie.Blazor.Medias/wwwroot/js.js
@@ -2,7 +2,7 @@ export var $blazor_media = {
     get(mediaId) {
         console.log(`media_get----------${mediaId}`);
         try {
-            var m = document.getElementById(mediaId);
+            let m = document.getElementById(mediaId);
             return {
                 autoplay: m.autoplay,
                 controls: m.controls,
@@ -32,28 +32,28 @@ export var $blazor_media = {
     setMuted(mediaId, muted) {
         console.log(`media_setMuted----------${mediaId}`);
         try {
-            var m = document.getElementById(mediaId);
+            let m = document.getElementById(mediaId);
             m && (m.muted = muted);
         } catch (exception) { }
     },
     setVolume(mediaId, volume) {
         console.log(`media_setVolume----------${mediaId}`);
         try {
-            var m = document.getElementById(mediaId);
+            let m = document.getElementById(mediaId);
             m && (m.volume = volume);
         } catch (exception) { }
     },
     setLoop(mediaId, loop) {
         console.log(`media_setLoop----------${mediaId}`);
         try {
-            var m = document.getElementById(mediaId);
+            let m = document.getElementById(mediaId);
             m && (m.loop = loop);
         } catch (exception) { }
     },
     setCurrentTime(mediaId, currentTime) {
         console.log(`media_setCurrentTime----------${mediaId}`);
         try {
-            var m = document.getElementById(mediaId);
+            let m = document.getElementById(mediaId);
             m && (m.currentTime = currentTime);
         } catch (exception) { }
     },
